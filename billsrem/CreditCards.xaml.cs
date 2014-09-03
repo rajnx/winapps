bdb920d3aa34b3698d1ef7505d859614d73d8ace
Rajneesh <rajnx@hotmail.com>
2014-09-03 16:29:19 -0700

    Adding initial drop for bills rem app

---
+++ billsrem/CreditCards.xaml.cs
@@ -0,0 +1,109 @@
+﻿using BillsReminder.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace BillsReminder
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class CreditCards : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public ObservableCollection<CreditCard> creditCards = new ObservableCollection<CreditCard>();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public CreditCards()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+
+            creditCards.Add(new CreditCard("ToysRus", "https://www.onlinecreditcenter6.com/consumergen2/login.do?subActionId=1000&clientId=tru&accountType=generic", ""));
+            creditCards.Add(new CreditCard("BankOfAmerica", "https://www.bankofamerica.com/sitemap/hub/signin.go", ""));
+            creditCards.Add(new CreditCard("Macys", "https://www.macys.com/account/signin", ""));
+            creditCards.Add(new CreditCard("Discover", "https://www.discovercard.com/cardmembersvcs/loginlogout/app/ac_main", ""));
+
+            this.DataContext = creditCards;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            // TODO: Assign a bindable collection of items to this.DefaultViewModel["Items"]
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void itemGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(typeof(WebBrowser), ((CreditCard)(e.ClickedItem)).BillPayPortalUrl);
+        }
+
+    }
+}
---
+++ billsrem/DataModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillsReminder
+{
+    public enum BillType
+    {
+        CreditCard,
+        Regular
+    }
+
+    public sealed class Bill
+    {
+        private string title;
+        private string subTitle;
+        private string imagePath;
+        private BillType billType;
+
+        public Bill(string title, string subtitle, string imagepath, BillType billsType)
+        {
+            this.title = title;
+            this.subTitle = subtitle;
+            this.imagePath = imagepath;
+            this.billType = billsType;
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+
+        public BillType BillType
+        {
+            get { return this.billType; }
+            set { this.billType = value; }
+        }
+
+        public string SubTitle
+        {
+            get { return this.subTitle; }
+            set { this.subTitle = value; }
+        }
+
+        public string ImagePath
+        {
+            get { return this.imagePath; }
+            set { this.imagePath = value; }
+        }
+    }
+
+    public sealed class CreditCard
+    {
+        private string creditCompanyName;
+        private string billUrl;
+        private string imagePath;
+
+        public CreditCard(string creditCompanyName, string billUrl, string imagePath)
+        {
+            this.creditCompanyName = creditCompanyName;
+            this.billUrl = billUrl;
+            this.imagePath = imagePath;
+        }
+
+        public string CompanyName
+        {
+            get { return this.creditCompanyName; }
+            set { this.creditCompanyName = value; }
+        }
+
+        public string BillPayPortalUrl
+        {
+            get { return this.billUrl; }
+            set { this.billUrl = value; }
+        }
+
+        public string ImagePath
+        {
+            get { return this.imagePath; }
+            set { this.imagePath = value; }
+        }
+    }
+}
